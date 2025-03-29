090234c6cfc93aef702486b9f596476b4097984b
Marcelo Augusto <123131698+marcelitos1v9@users.noreply.github.com>
2025-03-29 12:47:32 -0300

    Delete cms-projetos directory

--- cms-projetos/src/lib/db.js
+++
@@ -1,9 +0,0 @@
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
-
-export async function openDb() {
-  return open({
-    filename: './csm_db.sqlite3',
-    driver: sqlite3.Database,  
-  });
-}
